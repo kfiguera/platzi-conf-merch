2801448f783c01756c4259e3c46f8a975267ced9
Kevin Figuera <kevinalejandro3@gmail.com>
2022-08-17 19:31:12 -0400

    Estructura y creación de componentes para Platzi Conf Store

---
+++ src/routes/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from '../containers/Home';
+import Checkout from '../containers/Checkout';
+import Success from '../containers/Success';
+import Payment from '../containers/Payment';
+import Information from '../containers/Information';
+import NotFound from '../containers/NotFound';
+
+//import '../style/app.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/checkout' element={<Checkout />} />
+        <Route exact path='/checkout/information' element={<Information />} />
+        <Route exact path='/checkout/payment' element={<Payment />} />
+        <Route exact path='/checkout/success' element={<Success />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -52,5 +53,6 @@ module.exports = {
     compress: true,
     port: 3000,
     open: true,
+    historyApiFallback: true,
   },
 };
