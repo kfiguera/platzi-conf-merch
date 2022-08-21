3cb55d7a2821cd925763d5b31d8206bb5c03a858
Kevin Figuera <kevinalejandro3@gmail.com>
2022-08-21 15:47:39 -0400

    Creando nuestro primer custom hook

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,23 +7,28 @@ import Success from '../containers/Success';
 import Payment from '../containers/Payment';
 import Information from '../containers/Information';
 import NotFound from '../containers/NotFound';
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 
-import '../styles/components/App.css';
+//import '../styles/components/App.css';
 
 const App = () => {
+  const initialState = useInitialState();
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/checkout' element={<Checkout />} />
-          <Route exact path='/checkout/information' element={<Information />} />
-          <Route exact path='/checkout/payment' element={<Payment />} />
-          <Route exact path='/checkout/success' element={<Success />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/checkout' element={<Checkout />} />
+            <Route exact path='/checkout/information' element={<Information />} />
+            <Route exact path='/checkout/payment' element={<Payment />} />
+            <Route exact path='/checkout/success' element={<Success />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 export default App;
