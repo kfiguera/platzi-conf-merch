d27092852221d4ff89e5374fb4125e299b9ccb3a
Kevin Figuera <kevinalejandro3@gmail.com>
2022-08-21 23:30:31 -0400

    useContext en la página de checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../styles/components/Checkout.css';
+import AppContext from '../context/AppContext';
 import { Link } from 'react-router-dom';
 import { FaTrashAlt } from 'react-icons/fa';
 
 const Checkout = () => {
+  const { state, removeFromCart } = useContext(AppContext);
+  const { cart } = state;
+
+  const handleRemove = (product) => {
+    removeFromCart(product);
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    console.log(sum);
+    return sum;
+  };
+
   return (
     <div className='Checkout'>
       <div className='Checkout-content'>
-        <h3>Lista de Pedidos</h3>
-        <div className='Checkout-item'>
-          <div className='Checkout-element'>
-            <h4>ITEM name</h4>
-            <span>$10</span>
+        <h3>{cart.length > 0 ? 'Lista de Pedidos' : 'Sin pedidos'}</h3>
+        {cart.map((item) => (
+          <div className='Checkout-item'>
+            <div className='Checkout-element'>
+              <h4>{item.title}</h4>
+              <span>${item.price}</span>
+            </div>
+            <button type='button' onClick={() => handleRemove(item)}>
+              <FaTrashAlt title='Eliminar' />
+            </button>
           </div>
-          <button type="button">
-            <FaTrashAlt title='Eliminar' />
-          </button>
-        </div>
+        ))}
+
       </div>
+      {cart.length > 0 && (
       <div className='Checkout-sidebar'>
-        <h3>Precio Total: $10</h3>
-        <Link to="/checkout/information">
-          <button type="button">Continuar Pedido</button>
+        <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
+        <Link to='/checkout/information'>
+          <button type='button'>Continuar Pedido</button>
         </Link>
       </div>
+      )}
     </div>
   );
 };
